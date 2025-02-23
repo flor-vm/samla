de3084decacbf6dd6edd4c193a1153d0704fc627
flor-vm <flor.iveth.vm@gmail.com>
2025-02-23 12:31:39 -0600

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/',
+  server: {
+    host: true,
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    strictPort: true,
+    open: true,
+  },
   plugins: [react()],
   css: {
     modules: {
